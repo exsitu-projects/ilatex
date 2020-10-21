a386bedc48d3e7589e038534b564e8cf3635ee9f
Daru13 <daru14@gmail.com>
2020-10-21 23:49:29 +0200

    Refactor/clean the view of the tabular visualisation.
    This fixes various bugs and (finally!) improves the height of the visualisation, which should now be closer to the total height of the table.

--- src/visualisations/tabular/view/view.ts
+++ src/visualisations/tabular/view/view.ts
@@ -16,7 +16,6 @@ interface HandsontableCellCoords {
 
 interface ColumnDetail {
     type: string;
-    isResizable: boolean;
 }
 
 interface CellLocation {
@@ -32,108 +31,76 @@ class TabularView extends AbstractVisualisationView {
     private tableContent: string[][];
 
     private viewNode: HTMLElement;
+    private handsontableContainerNode: HTMLElement;
     private handsontableInstance: Handsontable | null;
-
+    private handsontableMutationObserver: MutationObserver | null;
+    
     constructor(contentNode: HTMLElement, context: VisualisationViewInstantiationContext) {
         super(contentNode, context);
 
-        this.columnDetails = [];
-        this.tableContent = [];
+        this.columnDetails = this.extractColumnDetails();
+        this.tableContent = this.extractTableContent();
 
+        this.handsontableContainerNode = document.createElement("div");
+        this.handsontableContainerNode = this.createHandsontableContainer();
         this.viewNode = document.createElement("div");
-        this.handsontableInstance = null;
+        this.viewNode.append(this.handsontableContainerNode);
 
-        this.updateWith(contentNode);
+        this.handsontableInstance = null;
+        this.handsontableMutationObserver = null; 
+        this.generateNewHandsontable();
     }
 
     // Extract column definitions from the table header
-    extractColumnDetails(): void {
+    private extractColumnDetails(): ColumnDetail[] {
+        const columnDetails = [];
+
         const header = this.contentNode.querySelector("thead")!;
         const headerCellNodes = Array.from(header.querySelectorAll("th"));
-
-        for (let headerCellNode of headerCellNodes) {
-            // Get the type of the column
-            const columnType = headerCellNode.textContent ?? "";
-
-            // Only columns whose cells are paragraphs with a fixed width can be resized
-            // TODO: if the column can be resized, get their initial size
-            const columnCanBeResized = ["p", "m", "b"]
-                .includes(columnType.charAt(0));
-
-            // Save information about the current column
-            this.columnDetails.push({
-                type: columnType,
-                isResizable: columnCanBeResized
+        for (let node of headerCellNodes) {
+            columnDetails.push({
+                type: node.textContent ?? ""
             });
         }
+
+        return columnDetails;
     }
 
     // Extract data from the regular table cells
-    extractTableContent(): void {
+    private extractTableContent(): string[][] {
+        const tableContent = [];
+
         const contentRowNodes = Array.from(this.contentNode.querySelectorAll("tbody > tr"));
         for (let contentRowNode of contentRowNodes) {
             // Create and fill a new row for the table content
             const row: string[] = [];
-            this.tableContent.push(row);
+            tableContent.push(row);
 
             const contentCellNodes = Array.from(contentRowNode.querySelectorAll("td"));
             for (let contentCellNode of contentCellNodes) {
                 row.push(contentCellNode.textContent!);
             }
         }
+
+        return tableContent;
     }
 
-    selectDocumentCellContent(cellLocation: CellLocation): void {
-        this.messenger.sendMessage({
-            type: WebviewToCoreMessageType.NotifyVisualisationModel,
-            visualisationId: this.visualisationId,
-            title: "select-cell-code",
-            notification: {
-                columnIndex: cellLocation.columnIndex,
-                rowIndex: cellLocation.rowIndex
-            }
-        });
+    private createHandsontableContainer(): HTMLElement {
+        const containerElement = document.createElement("div");
+        containerElement.classList.add("handsontable-container");
+
+        // Initial refining of the table container's height
+        // It will be finely adjusted once the table is appended to the DOM
+        containerElement.style.setProperty(
+            "--table-height",
+            `${(this.tableContent.length + 1) * 23}px`
+        );
+
+        return containerElement;
     }
 
-    setDocumentCellContent(cellLocation: CellLocation, newContent: string): void {
-        this.messenger.sendMessage({
-            type: WebviewToCoreMessageType.NotifyVisualisationModel,
-            visualisationId: this.visualisationId,
-            title: "set-cell-content",
-            notification: {
-                columnIndex: cellLocation.columnIndex,
-                rowIndex: cellLocation.rowIndex,
-                newContent: newContent
-            }
-        });
-    }
-
-    moveDocumentRow(oldRowIndex: number, newRowIndex: number): void {
-        this.messenger.sendMessage({
-            type: WebviewToCoreMessageType.NotifyVisualisationModel,
-            visualisationId: this.visualisationId,
-            title: "move-row",
-            notification: {
-                oldRowIndex: oldRowIndex,
-                newRowIndex: newRowIndex
-            }
-        });
-    }
-
-    moveDocumentColumn(oldColumnIndex: number, newColumnIndex: number): void {
-        this.messenger.sendMessage({
-            type: WebviewToCoreMessageType.NotifyVisualisationModel,
-            visualisationId: this.visualisationId,
-            title: "move-column",
-            notification: {
-                oldColumnIndex: oldColumnIndex,
-                newColumnIndex: newColumnIndex
-            }
-        });
-    }
-
-    createHandsontableInstance(): Handsontable {
-        return new Handsontable(this.viewNode, {
+    private createHandsontableInstance(): Handsontable {
+        return new Handsontable(this.handsontableContainerNode, {
             data: this.tableContent,
 
             colHeaders: (index: number) => this.columnDetails[index].type,
@@ -148,7 +115,7 @@ class TabularView extends AbstractVisualisationView {
         });
     }
 
-    startHandlingHandsontableEvents(): void {
+    private startHandlingHandsontableEvents(): void {
         if (!this.handsontableInstance) {
             console.error("Adding handsontables event handlers requires a Handsontable instance.");
             return;
@@ -215,7 +182,7 @@ class TabularView extends AbstractVisualisationView {
         }
     }
 
-    stopHandlingHandsontableEvents(): void {
+    private stopHandlingHandsontableEvents(): void {
         if (!this.handsontableInstance) {
             console.error("Removing handsontables event handlers requires a Handsontable instance.");
             return;
@@ -223,6 +190,105 @@ class TabularView extends AbstractVisualisationView {
 
         // Remove all the callbacks which (may) have been added to the current handsontable instance
         Handsontable.hooks.destroy(this.handsontableInstance);
+    }
+
+    private selectDocumentCellContent(cellLocation: CellLocation): void {
+        this.messenger.sendMessage({
+            type: WebviewToCoreMessageType.NotifyVisualisationModel,
+            visualisationId: this.visualisationId,
+            title: "select-cell-code",
+            notification: {
+                columnIndex: cellLocation.columnIndex,
+                rowIndex: cellLocation.rowIndex
+            }
+        });
+    }
+
+    private setDocumentCellContent(cellLocation: CellLocation, newContent: string): void {
+        this.messenger.sendMessage({
+            type: WebviewToCoreMessageType.NotifyVisualisationModel,
+            visualisationId: this.visualisationId,
+            title: "set-cell-content",
+            notification: {
+                columnIndex: cellLocation.columnIndex,
+                rowIndex: cellLocation.rowIndex,
+                newContent: newContent
+            }
+        });
+    }
+
+    private moveDocumentRow(oldRowIndex: number, newRowIndex: number): void {
+        this.messenger.sendMessage({
+            type: WebviewToCoreMessageType.NotifyVisualisationModel,
+            visualisationId: this.visualisationId,
+            title: "move-row",
+            notification: {
+                oldRowIndex: oldRowIndex,
+                newRowIndex: newRowIndex
+            }
+        });
+    }
+
+    private moveDocumentColumn(oldColumnIndex: number, newColumnIndex: number): void {
+        this.messenger.sendMessage({
+            type: WebviewToCoreMessageType.NotifyVisualisationModel,
+            visualisationId: this.visualisationId,
+            title: "move-column",
+            notification: {
+                oldColumnIndex: oldColumnIndex,
+                newColumnIndex: newColumnIndex
+            }
+        });
+    }
+
+    private startObservingHandsontableMutations() {
+        this.handsontableMutationObserver = new MutationObserver((mutations) => {
+            for (let mutation of mutations) {
+                // Since we only seek to observe style changesm
+                // we only want to react to attribute changes 
+                if (mutation.type === "attributes") {
+                    this.resizeHandsontableContainer();
+                }
+            }
+        });
+        
+        // The element to observe is a descendant of the Handsontable top-level container
+        // cf. the explanation given in resizeHandsontableContainer's method definition
+        const targetElement = this.handsontableContainerNode.querySelector(".wtHider")! as HTMLElement;
+        this.handsontableMutationObserver.observe(targetElement, { attributes: true });
+    }
+
+    private stopObservingHandsontableMutations() {
+        if (!this.handsontableMutationObserver) {
+            console.error("The Handsontable's mutation observer can only be removed if it exists.");
+            return;
+        }
+
+        this.handsontableMutationObserver.disconnect();
+    }
+
+    // Note: this requires the table to be appended to the DOM
+    private resizeHandsontableContainer(): void {
+        // Handsontable appears to set the height of the node
+        // with the "htHider" class to the total height of the table.
+        // This height can therefore be used to resize the container
+        // of the current Handsontable instance and update the
+        // dimensions of the table (so that it uses it).
+        const wtHiderElement = this.handsontableContainerNode.querySelector(".wtHider")! as HTMLElement;
+        const totalTableHeight = wtHiderElement.getBoundingClientRect().height;
+
+        this.handsontableContainerNode.style.setProperty(
+            "--table-height",
+            `${totalTableHeight + 0}px`
+        );
+        this.handsontableInstance.refreshDimensions();
+    }
+
+    private generateNewHandsontable(): void {
+        this.handsontableInstance = this.createHandsontableInstance();
+
+        this.startHandlingHandsontableEvents();
+        this.startObservingHandsontableMutations();
     }
 
     render(): HTMLElement {
@@ -234,32 +300,25 @@ class TabularView extends AbstractVisualisationView {
         
         this.contentNode = newContentNode;
 
-        // Create and populate fresh data structures from the new content node
-        this.columnDetails = [];
-        this.tableContent = [];
+        // Extract fresh data fron the new content node
+        this.columnDetails = this.extractColumnDetails();
+        this.tableContent = this.extractTableContent();
 
-        this.extractColumnDetails();
-        this.extractTableContent();
+        // Create and configure a new container for the future new table
+        this.handsontableContainerNode = this.createHandsontableContainer();
 
-        // Create a new empty view node and a new Handonstable instance
-        this.viewNode = document.createElement("div");
-
-        // TODO: move this elsewhere/remove this
-        this.viewNode.innerHTML = "";
-        this.viewNode.style.height = `${this.tableContent.length * 30}px`;
-        this.viewNode.style.width = "100%";
-        this.viewNode.style.overflow = "hidden";
-        this.viewNode.style.padding = "0";
-
-        this.tableContent.splice(5, 1); // TEMPOARY FIX (TODO: fix and remove)
-
-        // Remove old Handsontable event handlers before ditching the old handsontable instance
-        // and add new handlers to the newly created instance
+        // Remove the old event listeners and mutation observers
+        // before creating a new Handsontable instance
         this.stopHandlingHandsontableEvents();
-        this.handsontableInstance = this.createHandsontableInstance();
-        this.startHandlingHandsontableEvents();
+        this.stopObservingHandsontableMutations();
+
+        // Replace the content of the old view node by the new container
+        this.viewNode.innerHTML = "";
+        this.viewNode.append(this.handsontableContainerNode);
+
+        // Populate the new container with a new table
+        this.generateNewHandsontable();
     }
-    
 }
 
 export class TabularViewFactory implements VisualisationViewFactory {
