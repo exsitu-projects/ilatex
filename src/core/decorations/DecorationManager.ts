e315a948bfa58b52e3871b501ed621ba4b1494a8
Daru13 <daru14@gmail.com>
2021-01-30 20:29:05 +0100

    Refactor/clean up the decoration manager

--- src/core/decorations/DecorationManager.ts
+++ src/core/decorations/DecorationManager.ts
@@ -2,7 +2,7 @@ import * as vscode from "vscode";
 import { ArrayMap } from "../../shared/utils/ArrayMap";
 import { InteractiveLatex } from "../InteractiveLaTeX";
 import { VisualisationModel } from "../visualisations/VisualisationModel";
-import { decorationTypes } from "./decoration-types";
+import { textDecorations } from "./text-decorations";
 
 interface ModelWithEditor {
     model: VisualisationModel;
@@ -12,37 +12,44 @@ interface ModelWithEditor {
 export class DecorationManager {
     private ilatex: InteractiveLatex;
 
-    private onRedecorateEventEmitter: vscode.EventEmitter<void>;
+    private onRedecorateEditorsEventEmitter: vscode.EventEmitter<void>;
     private codeLensProviderDisposable: vscode.Disposable;
 
     constructor(ilatex: InteractiveLatex) {
         this.ilatex = ilatex;
 
-        this.onRedecorateEventEmitter = new vscode.EventEmitter<void>();
-
-        const self = this;
-        this.codeLensProviderDisposable = vscode.languages.registerCodeLensProvider(
-            { scheme: "file", language:"latex" },
-            {
-                provideCodeLenses(document: vscode.TextDocument) {
-                    return self.ilatex.visualisationModelManager.models
-                        .filter(model => model.sourceFile.absolutePath === document.uri.path && model.hasBeenManuallyEdited)
-                        .map(model =>  new vscode.CodeLens(model.codeRange, {
-                            title: "iLaTeX is out-of-sync with this piece of code. Click to recompile the document and fix the problem.",
-                            command: "ilatex.recompile"
-                        }));
-                },
-
-                onDidChangeCodeLenses: self.onRedecorateEventEmitter.event
-            }
-        );
+        this.onRedecorateEditorsEventEmitter = new vscode.EventEmitter<void>();
+        this.codeLensProviderDisposable = this.createCodeLensesProvider();
+        
     }
 
     dispose(): void {
         this.codeLensProviderDisposable.dispose();
     }
 
-    private mapVisibleEditorsToVisualisationModels(models: VisualisationModel[]): ArrayMap<vscode.TextEditor, VisualisationModel> {
+    private createCodeLensesProvider(): vscode.Disposable {
+        return vscode.languages.registerCodeLensProvider(
+            {
+                scheme: "file",
+                language:"latex"
+            },
+            {
+                provideCodeLenses: (document) => this.computeCodeLensesForDocument(document),
+                onDidChangeCodeLenses: this.onRedecorateEditorsEventEmitter.event
+            }
+        );
+    }
+
+    private computeCodeLensesForDocument(document: vscode.TextDocument): vscode.CodeLens[] {
+        return this.ilatex.visualisationModelManager.models
+            .filter(model => model.sourceFile.absolutePath === document.uri.path && model.hasBeenManuallyEdited)
+            .map(model =>  new vscode.CodeLens(model.codeRange, {
+                title: "iLaTeX is out-of-sync with this piece of code. Click to recompile the document and fix the problem.",
+                command: "ilatex.recompile"
+            }));
+    }
+
+    private mapVisibleEditorsToVisualisations(models: VisualisationModel[]): ArrayMap<vscode.TextEditor, VisualisationModel> {
         // Map each visible editor to 0+ visualisation models
         const visibleEditors = vscode.window.visibleTextEditors;
         const modelsWithVisibleEditors: ModelWithEditor[] = models
@@ -63,33 +70,24 @@ export class DecorationManager {
     }
 
     private redecorateEditorWithVisualisations(editor: vscode.TextEditor, models: VisualisationModel[]): void {
-        const visualisableCodeRanges: vscode.Range[] = models
-            .filter(model => !model.hasBeenManuallyEdited)
-            .map(model => model.codeRange);
-        editor.setDecorations(decorationTypes.visualisableCode, visualisableCodeRanges);
-        
         const manuallyEditedVisualisableCodeRanges: vscode.Range[] = models
             .filter(model => model.hasBeenManuallyEdited)
             .map(model => model.codeRange);
-        editor.setDecorations(decorationTypes.manuallyEditedVisualisableCode, manuallyEditedVisualisableCodeRanges);
-
-        // console.log("Redecorated the following ranges:", visualisableCodeRanges);
+        editor.setDecorations(textDecorations.manuallyEditedVisualisableCode, manuallyEditedVisualisableCodeRanges);
     }
 
     private redecorateVisibleEditorsWithVisualisations(models: VisualisationModel[]): void {
-        const visibleEditorsToModels = this.mapVisibleEditorsToVisualisationModels(models);
+        const visibleEditorsToModels = this.mapVisibleEditorsToVisualisations(models);
 
         // For each editor, redecorate it using the models located in the source file it displays
         for (let [editor, models] of visibleEditorsToModels.entries) {
             this.redecorateEditorWithVisualisations(editor, models);
         }
 
-        this.onRedecorateEventEmitter.fire();
+        this.onRedecorateEditorsEventEmitter.fire();
     }
 
     redecorateVisibleEditorsWithCurrentVisualisations(): void {
-        this.redecorateVisibleEditorsWithVisualisations(
-            this.ilatex.visualisationModelManager.models
-        );
+        this.redecorateVisibleEditorsWithVisualisations(this.ilatex.visualisationModelManager.models);
     }
 }
--- src/core/decorations/text-decorations.ts
+++ src/core/decorations/text-decorations.ts
@@ -1,12 +1,6 @@
 import * as vscode from "vscode";
 
-export const decorationTypes = {
-    // Styles of decorations of pieces of code that can be visualised
-    visualisableCode: vscode.window.createTextEditorDecorationType({
-        backgroundColor: "rgba(20, 20, 255, 0.2)",
-        borderRadius: "2px"
-    }),
-
+export const textDecorations = {
     // Styles of decorations of pieces of code that used to be visualisable
     // and that have been manually edited by the user
     manuallyEditedVisualisableCode: vscode.window.createTextEditorDecorationType({
