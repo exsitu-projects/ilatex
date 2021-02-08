91936c523ce379b6b08d6eb9155b6bf2cd43770e
Daru13 <daru14@gmail.com>
2021-02-08 11:58:27 +0100

    Update the model of the vis. of math. environments

--- src/visualisations/mathematics/model/model.ts
+++ src/visualisations/mathematics/model/model.ts
@@ -1,21 +1,26 @@
 import * as vscode from "vscode";
-import { VisualisationModelFactory, VisualisationModel, VisualisationModelUtilities } from "../../../core/visualisations/VisualisationModel";
-import { AbstractVisualisationModel, NotificationHandlerSpecification } from "../../../core/visualisations/AbstractVisualisationModel";
-import { ASTNode, ASTEnvironementNode, ASTNodeType } from "../../../core/ast/LatexASTNode";
-import { CodeMapping } from "../../../core/mappings/CodeMapping";
+import { AbstractVisualisationModel, ViewMessageHandlerSpecification } from "../../../core/visualisations/AbstractVisualisationModel";
+import { EnvironmentNode } from "../../../core/ast/nodes/EnvironmentNode";
+import { VisualisableCodeContext } from "../../../core/visualisations/VisualisationModelProvider";
+import { VisualisationModelUtilities } from "../../../core/visualisations/VisualisationModelUtilities";
+import { WhitespaceNode } from "../../../core/ast/nodes/WhitespaceNode";
 
+export class MathematicsModel extends AbstractVisualisationModel<EnvironmentNode> {
+    readonly name = "mathematics";
+    private trimmedMathCode: string | null;
 
-class MathematicsModel extends AbstractVisualisationModel<ASTEnvironementNode> {
-    static readonly visualisationName = "mathematics";
-    readonly visualisationName = MathematicsModel.visualisationName;
-
-    constructor(node: ASTEnvironementNode, mapping: CodeMapping, utilities: VisualisationModelUtilities) {
-        super(node, mapping, utilities);
+    constructor(context: VisualisableCodeContext<EnvironmentNode>, utilities: VisualisationModelUtilities) {
+        super(context, utilities);
+        this.trimmedMathCode = null;
     }
 
-    protected createNotificationHandlerSpecifications(): NotificationHandlerSpecification[] {
+    protected get contentDataAsHtml(): string {
+        return this.trimmedMathCode || "";
+    }
+
+    protected get viewMessageHandlerSpecifications(): ViewMessageHandlerSpecification[] {
         return [
-            ...super.createNotificationHandlerSpecifications(),
+            ...super.viewMessageHandlerSpecifications,
 
             {
                 title: "set-math-code",
@@ -27,17 +32,30 @@ class MathematicsModel extends AbstractVisualisationModel<ASTEnvironementNode> {
         ];
     }
 
+    protected async updateContentData(): Promise<void> {
+        try {
+            this.trimmedMathCode = await this.astNode.body.textContent;
+            this.contentUpdateEndEventEmitter.fire(true);
+        }
+        catch (error) {
+            console.log(`The content data update of the visualisation with UID ${this.uid} (${this.name}) failed.`);
+            this.contentUpdateEndEventEmitter.fire(false);
+
+        }
+    }
+
     private async setNewMathCode(trimmedMathCode: string): Promise<void> {
-        const editor = await this.codeMapping.sourceFile.getOrDisplayInEditor();
+        const editor = await this.sourceFile.getOrOpenInEditor();
 
         // Find the first and the last non-whitespace AST nodes in the environement
         // and replace the code between the start of the first node and the end of the second one
         // If there is non (i.e. only whitespace or nothing), simply replace the whole content
-        const environementContentNodes = this.astNode.value.content.value as ASTNode[];
-        const firstNonWhitespaceNode = environementContentNodes.find(node => node.type !== ASTNodeType.Whitespace);
-        const lastNonWhitespaceNode = [...environementContentNodes] // Clone the array because .reverse() is performed in-place
+        const environementBodyNodes = [...this.astNode.body.childNodes];
+        const firstNonWhitespaceNode = environementBodyNodes
+            .find(node => !(node instanceof WhitespaceNode));
+        const lastNonWhitespaceNode = environementBodyNodes
             .reverse()
-            .find(node => node.type !== ASTNodeType.Whitespace);
+            .find(node => !(node instanceof WhitespaceNode));
 
         const rangeToEdit = (firstNonWhitespaceNode !== undefined && lastNonWhitespaceNode !== undefined)
             ? new vscode.Range(
@@ -45,40 +63,12 @@ class MathematicsModel extends AbstractVisualisationModel<ASTEnvironementNode> {
                 lastNonWhitespaceNode.range.to.asVscodePosition
             )
             : new vscode.Range(
-                this.astNode.value.content.range.from.asVscodePosition,
-                this.astNode.value.content.range.to.asVscodePosition
+                this.astNode.body.range.from.asVscodePosition,
+                this.astNode.body.range.to.asVscodePosition
             );
 
         await editor.edit(editBuilder => {
             editBuilder.replace(rangeToEdit, trimmedMathCode);
         });
-    }
-
-    protected renderContentAsHTML(): string {
-        const fileContent = this.codeMapping.sourceFile.readContentSync();
-        const mathsAsText = fileContent.substring(
-            this.astNode.value.content.range.from.offset,
-            this.astNode.value.content.range.to.offset
-        );
-
-        return mathsAsText;
-
-        // const mathsAsKatexHtml = katex.renderToString(mathsAsText, {
-        //     throwOnError: false
-        // });
-
-        // return mathsAsKatexHtml;
-    }
-}
-
-export class MathematicsModelFactory implements VisualisationModelFactory {
-    readonly visualisationName = MathematicsModel.visualisationName;
-    readonly astMatchingRule = (node: ASTNode) => {
-        return node.type === ASTNodeType.Environement
-            && node.name === "imaths";
-    };
-
-    createModel(node: ASTNode, mapping: CodeMapping, utilities: VisualisationModelUtilities): VisualisationModel {
-        return new MathematicsModel(node as ASTEnvironementNode, mapping, utilities);
     }
 }
---
+++ src/visualisations/mathematics/model/provider.ts
@@ -0,0 +1,29 @@
+import { ASTNode } from "../../../core/ast/nodes/ASTNode";
+import { EnvironmentNode } from "../../../core/ast/nodes/EnvironmentNode";
+import { CodeMapping } from "../../../core/code-mappings/CodeMapping";
+import { VisualisationModelProvider, VisualisableCodeContext } from "../../../core/visualisations/VisualisationModelProvider";
+import { VisualisationModelUtilities } from "../../../core/visualisations/VisualisationModelUtilities";
+import { MathematicsModel } from "./model";
+
+
+export class MathematicsVisualisationModelProvider implements VisualisationModelProvider {
+    canProvideForCodeMapping(mapping: CodeMapping): boolean {
+        return mapping.type === "imaths";
+    }
+
+    canProvideForASTNode(node: ASTNode): boolean {
+        return node instanceof EnvironmentNode
+            && node.name === "imaths";
+    }
+
+    provideModelWith(
+        context: VisualisableCodeContext<ASTNode>,
+        utilities: VisualisationModelUtilities
+    ): MathematicsModel {
+        return new MathematicsModel(
+            context as VisualisableCodeContext<EnvironmentNode>,
+            utilities
+        );
+    };
+}
+
