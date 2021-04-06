bab4adb8fdccbc8061f2153166979c3ee9c09eb2
Daru13 <daru14@gmail.com>
2021-04-06 22:50:29 +0200

    Add a EveryPossibleKeysOf utility type

--- src/shared/utils/type-utils.ts
+++ src/shared/utils/type-utils.ts
@@ -1,3 +1,7 @@
 /** Ignore readonly modifiers on the given type. */
 // From https://stackoverflow.com/a/43001581
 type Writeable<T> = { -readonly [P in keyof T]: T[P] };
+
+/** Get all the possible key of the given type (e.g. of a disjoint union type). */
+// From https://stackoverflow.com/a/49402091
+type EveryPossibleKeysOf<T> = T extends T ? keyof T: never;
