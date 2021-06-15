d8b5f9bb0032dbd93df91542b5f8ab67f721ce19
Daru13 <daru14@gmail.com>
2021-06-15 12:07:56 +0200

    Use the official webview API types
    The type are now available via the @types/vscode-webview package.

--- src/webview/VisualCodeStudioApi.ts
+++ src/webview/VisualCodeStudioApi.ts
@@ -1,8 +1,4 @@
-// Declare the type of the global 'acquireVsCodeApi' function provided by VSCode
-// Should the webview code require other fields of the returned object, it should be updated here
-declare function acquireVsCodeApi(): {
-    postMessage: (message: any) => void;
-};
+import "vscode-webview";
 
 // Request an access to the VSCode API
 export const vscode = acquireVsCodeApi();
